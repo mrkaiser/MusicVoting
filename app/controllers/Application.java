5b49114a1f88f341945cf7891c84667abb5de0b0
Dev Gupta <kaiserollofdoom@gmail.com>
2012-11-30 20:11:36 -0500

    Stubbed methods
    I stubbed out the three primary methods the overall application. The
    rest will probably be backend.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,4 +11,18 @@ public class Application extends Controller {
     return ok(index.render("Your new application is ready."));
   }
   
+  public static Result vote(String id){
+	  //TODO push vote to queue
+	  return ok(id);
+  }
+  
+  public static Result getQueue(Integer length){
+	  //The length is the length of the queue returned
+	  return ok("The queue is "+length+" long");
+  }
+  
+  public static Result auto(){
+	  //Set the autopilot
+	  return ok("Toggle Toggle");
+  }
 }
